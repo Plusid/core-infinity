8e17a6dc702cb2c10057c8d249ca22a5d0816a81
Infinity Software Ltd <31319908+Plusid@users.noreply.github.com>
2020-11-16 18:54:39 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -214,9 +214,9 @@ alias ark="$HOME/core-bridgechain/packages/core/bin/run"
 echo 'alias infinity="$HOME/core-bridgechain/packages/core/bin/run"' >> ~/.bashrc
 
 rm -rf "$HOME/core-bridgechain"
-git clone "https://github.com/Plusid/core-master" "$HOME/core-bridgechain" || FAILED="Y"
+git clone "https://github.com/Plusid/core-infinity" "$HOME/core-bridgechain" || FAILED="Y"
 if [ "$FAILED" == "Y" ]; then
-    echo "Failed to fetch core repo with origin 'https://github.com/Plusid/core-master'"
+    echo "Failed to fetch core repo with origin 'https://github.com/Plusid/core-master-infinity'"
 
     exit 1
 fi
